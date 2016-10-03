4827b861c8c7fff4eca88e72538d556bed974643
rajesh_naroth <rajesh.naroth@oracle.com>
2016-10-03 11:27:53 -0700

    UMD fixes

--- examples/one.js
+++ examples/one.js
@@ -1,6 +1,7 @@
 'use strict';
 import Choreo from '../src/choreo'
 
+console.log('examples/one.js start')
 let sequence = Choreo.create()
 sequence.add(() => {console.log("step 1"); return '1'})
 sequence.wait(1000)
@@ -9,4 +10,4 @@ sequence.wait(2000)
 sequence.add((v) => {console.log("step 3", v); return v + '3'})
 sequence.start()
 
-console.log("one.js: ", 'done')
+console.log('examples/one.js: done')
